d89aa98f720768c593ed29599942db3087097fa9
Gelis <wergelis@msn.com>
2024-06-20 15:15:31 +0800

    完成使用舊版 Microsoft.AspNetCore.Mvc.Core 套件範例程式碼

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# 使用舊版 Microsoft.AspNetCore.Mvc.Core 套件範例程式碼
+
+## UseAuthExtensionsLab
---
+++ UseAuthExtensionsLab/PageModelBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UseAuthExtensionsLab
+{
+    /// <summary>
+    /// 在類別庫裡衍伸 Razor Page 的基底類別
+    /// </summary>
+    public class PageModelBase: PageModel
+    {
+
+    }
+}
